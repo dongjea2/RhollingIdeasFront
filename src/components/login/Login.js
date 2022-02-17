fb2de4887134a15048107156eb075c3b5f4ad52d
Jihoon Oh <bs4079.dev@gmail.com>
2022-02-17 12:13:39 +0900

    로그인 회원가입 기본틀

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,3 +1,66 @@
+import  rholling  from '../../images/mainpage/rholling.PNG';
+import { Link } from 'react-router-dom';
+import './Login.css';
+import { useRef } from 'react';
+
 export default function Login() {
-    return <h1>로그인 테스트 페이지</h1>;
+
+    const idRef = useRef(null);
+    const pwdRef = useRef(null);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let id = idRef.current.value;
+        let pwd = pwdRef.current.value;
+        console.log(id + " " + pwd);
+        fetch('url넣어야됨', {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                id : idRef,
+                pwd : pwdRef
+            })
+        })
+        .then(data => data.json())
+    }
+
+    return (
+    <div className='login-page'>
+        {/* 상단 로고(클릭시 메인페이지 이동) */}
+        <div className="logo">
+        <Link to="/">
+            <img src={rholling}/>
+        </Link>
+        </div>
+
+        {/* 로그인 */}
+        <div className="login_box">
+            <div className="login_box2">
+                <h3>로그인</h3>
+                <div className="sns_login">
+                        <button>페이스북 아이디로 로그인</button>
+                        <button>네이버 아이디로 로그인</button>
+                        <button>카카오톡 아이디로 로그인</button>
+                </div>
+                <hr/>
+                <form className= "email_login">
+                    <input type="text" ref={idRef} placeholder="이메일 주소 입력" required/>
+                    <input type="password" ref={pwdRef} placeholder="비밀번호 입력" required/>
+                    <button onClick={handleSubmit}>로그인</button>
+                </form>
+                <div className="login_text">
+                    아직 Rholling ideas 계정이 없으신가요?  
+                    <Link to={'/signup'}>가입하기</Link>
+                </div>
+                <hr/>
+                <div className="login_text">
+                    <Link to={''}>혹시 비밀번호를 잊으셨나요?</Link>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
--- src/components/login/Singup.js
+++ src/components/login/Singup.js
@@ -1,100 +1,74 @@
 import  rholling  from '../../images/mainpage/rholling.PNG';
 import { Link } from 'react-router-dom';
 import './Signup.css';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function Signup() {
 
-    const INIT_VALUES = {
-        name: '',
-        id: '',
-        id2: '',
-        pwd: '',
-        pwd2: ''
-    }
+    const nameRef = useRef(null);
+    const idRef = useRef(null);
+    const id2Ref = useRef(null);
+    const pwdRef = useRef(null);
+    const pwd2Ref = useRef(null);
 
-    const [values, setValues] = useState(INIT_VALUES);
-
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setValues((preValue) => ({
-            ...preValue,
-            [name]: value
-        }))
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(values);
-        
+
+        let name = nameRef.current.value;
+        let id = idRef.current.value;
+        let id2 = id2Ref.current.value;
+        let pwd = pwdRef.current.value;
+        let pwd2 = pwd2Ref.current.value;
+
+        if(id === id2 && pwd === pwd2){
+            fetch(`url 넣어야 됨`, {
+                method : 'POST',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify({
+                    name : name,
+                    id : id,
+                    pwd : pwd
+                })
+            })
+        } else {
+            console.log('이메일 또는 비밀번호가 일치하지 않습니다');
+        }
     }
 
     return(
-        <>
-        {/* 상단 로고(클릭시 메인페이지 이동) */}
-        <div className="logo">
-        <Link to="/">
-            <img src={rholling}/>
-        </Link>
-        </div>
+        <div className='signup-page'>
+            {/* 상단 로고(클릭시 메인페이지 이동) */}
+            <div className="logo">
+            <Link to="/">
+                <img src={rholling}/>
+            </Link>
+            </div>
 
-        {/* 회원가입 inputbox */}
-        <div className="signup_box">
-            <form className="signup_box2" onSubmit={handleSubmit}>
-                <h3>가입하기</h3>
-                이름<br/>
-                <input 
-                    type="text" 
-                    name="name" 
-                    value={values.name} 
-                    onChange={handleChange} 
-                    placeholder="사용하실 이름을 입력해주세요" 
-                    required
-                />
-                이메일 주소<br/>
-                <input 
-                    type="text" 
-                    name="id" 
-                    value={values.id} 
-                    onChange={handleChange} 
-                    placeholder="이메일 주소를 입력해주세요" 
-                    required
-                />
-                <input 
-                    type="text" 
-                    name="id2" 
-                    value={values.id2} 
-                    onChange={handleChange} 
-                    placeholder="이메일 주소를 확인합니다" 
-                    required
-                />
-                비밀번호<br/>
-                <input 
-                    type="password" 
-                    name="pwd" 
-                    value={values.pwd} 
-                    onChange={handleChange} 
-                    placeholder="비밀번호를 입력해주세요" 
-                    required
-                />
-                <input 
-                    type="password" 
-                    name="pwd2" 
-                    value={values.pwd2} 
-                    onChange={handleChange} 
-                    placeholder="비밀번호를 확인합니다" 
-                    required
-                />
-                <button id="signup_button" type='submit'>가입하기</button>
-            </form>
-            <hr/>
+            {/* 회원가입 inputbox */}
+            <div className="signup_box">
+                <form className="signup_box2">
+                    <h3>가입하기</h3>
+                    이름<br/>
+                    <input type="text" ref={nameRef} placeholder="사용하실 이름을 입력해주세요" required />
+                    이메일 주소<br/>
+                    <input type="text" ref={idRef} placeholder="이메일 주소를 입력해주세요" required />
+                    <input type="text" ref={id2Ref} placeholder="이메일 주소를 확인합니다" required />
+                    비밀번호<br/>
+                    <input type="password" ref={pwdRef} placeholder="비밀번호를 입력해주세요" required />
+                    <input type="password" ref={pwd2Ref} placeholder="비밀번호를 확인합니다" required />
+                    <button id="signup_button" onClick={handleSubmit}>가입하기</button>
+                </form>
+                <hr/>
 
-            {/* 로그인 페이지로 이동 */}
-            <div>
-                이미 Rholling Ideas 계정이 있으신가요?
-                <Link to="/login">기존 계정으로 로그인하기</Link>
+                {/* 로그인 페이지로 이동 */}
+                <div>
+                    이미 Rholling Ideas 계정이 있으신가요?<br/>
+                    <Link to="/login">기존 계정으로 로그인하기</Link>
+                </div>
             </div>
         </div>
-        </>
     );
 }
