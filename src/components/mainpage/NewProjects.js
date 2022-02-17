cd379423102a56b387b169529a317731a3deb70e
dongjea2 <dewawd@gmail.com>
2022-02-17 09:52:28 +0900

    신규 프로젝트 컴포넌트 구현

---
+++ src/components/mainpage/NewProjects.js
@@ -0,0 +1,22 @@
+import styles from './AttentionProjects.module.css';
+import ProjectMini from '../project/ProjectMini';
+import items from '../../api/mock/projectMock.json';
+
+
+export default function AttentionProjects() {
+  return (
+<div className={styles.fundingList}>
+    <span className={styles.listTitle}>신규 프로젝트</span>
+    <div className={styles.itemRapper}>
+        <div className={styles.itemInrap}>
+
+            {items.map((item) => (
+            <div className={styles.item} key={item.id}>
+                <ProjectMini item={item}/>
+            </div>
+            ))}
+        </div>
+    </div>
+</div>
+  );
+}
