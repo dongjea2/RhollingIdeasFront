b9c75358f80578fae1615b4bd66beddd9b029bb0
2reesh <dltkdgns1125@gmail.com>
2022-02-18 12:18:56 +0900

    글쓰기 폼 추가

--- src/components/project/Post.js
+++ src/components/project/Post.js
@@ -3,28 +3,50 @@ import React, {Component, useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import Comments from './Comment';
 
-const Community = () =>{
-    const [info, setInfo] = useState();
 
-}
-
-
-
-
-export default function Post() {
-
+export default function Post() {    
     const { prodNo } = useParams();
    
+    const postInfo = {
+        userNo:'',
+        postNo:'',
+        postCon:'',
+        postDate:''
+    } 
+
+    const [post, setPost] = useState(postInfo);
+
+    
+    const handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      };
+    
+    const submitClick = () => {
+        console.log(post);
+        this.setState({
+            userNo:'',
+            postNo:'',
+            postCon:'',
+            postDate:''
+        });
+      };
+
+      let userid = window.localStorage.getItem('userid');
+
+
         return ( 
         <>
             <div>
              프로젝트 {prodNo}의 커뮤니티
-                <div>게시글번호</div>
-                <div>게시글내용</div>
-                <div> 
-                    입력<input type={"text"} placeholder={"커뮤니티 게시글 작성"}></input> 
+                <div style={{display:'none'}}>게시글번호{postInfo.postNo}</div>
+                <div>유저이름{postInfo.userNo}</div>
+                <div>게시글내용{postInfo.postCon}</div>
+                <div className='postWrite' > 
+                    입력<input type={"text"} placeholder={"커뮤니티 게시글 작성"} defaultValue={post.postCon} onClick={handleChange}></input> 
+                    <button type='submit' onClick={submitClick}>작성</button>
                 </div>
-                <button>작성</button>
             </div>
                 <Comments></Comments>
         </> 
---
+++ src/components/project/ProjectWrite.js
@@ -0,0 +1,83 @@
+import React,{Component, useEffect, useRef, useState} from "react";
+
+export default function ProjectWrite(){
+    const [category, setCategory]= useState([]);
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(res=>{
+            return res.json();
+        })
+        .then(date =>{
+            setCategory(category);
+        });
+    }, []);
+
+
+    const projectnoRef = useRef(null);
+    const usernoRef = useRef(null);
+    const categoryRef = useRef(null);
+    const longtitleRef = useRef(null);
+    const projectbriefRef = useRef(null);
+    const editorpickRef = useRef(null);
+    const projectimageRef = useRef(null);
+    const targetpriceRef = useRef(null);
+    const startdateRef = useRef(null);
+    const enddateRef = useRef(null);
+    const shorttitleRef = useRef(null);
+    const projectcontentRef = useRef(null);
+    const projecturlRef = useRef(null);
+
+
+    function onSubmit(e){
+        e.preventDefault();
+        
+    fetch(``, {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            userNo : usernoRef.current.value,
+            category: categoryRef.current.value,
+            longtitle: longtitleRef.current.value,
+            projectbrief: projectbriefRef.current.value,
+            editorpick: editorpickRef.current.value,            
+            projectimage: projectimageRef.current.value,
+            targetprice: targetpriceRef.current.value,
+            startdate: startdateRef.current.value,
+            enddate: enddateRef.current.value,
+            shorttitle: shorttitleRef.current.value,            
+            projectcontent: projectcontentRef.current.value,
+            projecturl: projecturlRef.current.value        
+        }),
+    }).then(res=>{
+        if(res.ok){
+            alert("생성 완료")
+        }
+    })
+    }
+
+    return(
+        <form onSubmit={onSubmit}>
+            <select>
+                <option>
+                category  
+                </option>
+            </select>
+            <div className="input">
+            <div>projectNo</div>
+            <div>userNo</div>
+            
+            <input type="text" placeholder="longtitle"/>
+            <input type="text" placeholder="projectbrief"/>
+            <div>editorpick</div>
+            <div>projectimage</div>
+            <input type="number" placeholder="targetprice"/>
+            <input type="text" placeholder="shorttitle" />
+            <input type="text" placeholder="projectcontent"/>
+            <div>projecturl</div>
+            </div>
+        </form>
+    );
+}
