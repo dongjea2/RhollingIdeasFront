d00ea265b8edf92f310e79e6bb5d5d43f18864b4
2reesh <dltkdgns1125@gmail.com>
2022-02-23 18:43:34 +0900

    게시판 완성

--- src/components/project/Post.js
+++ src/components/project/Post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {Component, useEffect, useState} from 'react';
+import React, {Component, useEffect, useRef, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import Comments from './Comment';
 
@@ -7,70 +7,59 @@ import Comments from './Comment';
 export default function Post() {    
     const { prodNo } = useParams(); 
 
-    const [post, setPost] = useState(postInfo);
+    const [post, setPost] = useState('');
     
     const usernoRef = useRef(null);
-    const postnoRef = useRef(null);
     const postconRef = useRef(null);
-    const postdateRef = useRef(null);
     
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        let user = usernoRef.current.value;
-        let post = postnoRef.current.value;
+        let userno = usernoRef.current.value;
         let postcon = postconRef.current.value;
-        let post = postdateRef.current.value;
 
-            fetch(`url 넣어야 됨`, {
-                method : 'POST',
-                headers : {
-                    'Content-Type' : 'application/json'
-                },
-                body : JSON.stringify({
-                    name : name,
-                    id : id,
-                    pwd : pwd
-                })
-            })
-          }
+        let data = {
+          project: {projectNo: prodNo},
+          maker: {userNo: 1},
+          postContent: postcon
+        }
 
+        //console.log(data);
 
+        axios.post("/projectdetail/post",  JSON.stringify(data), {
+          headers: {
+            "Content-Type": `application/json`,
+          },
+        }).then((res) => {
+          console.log(res);
+        }).catch(err => console.log(err));
+      }
 
-    const submitClick = async (e) => {
-        console.log(post);
-        this.setState({
-            userNo:'',
-            postNo:'',
-            postCon:'',
-            postDate:''
-        });
-      };
-
-      let userid = window.localStorage.getItem('userid');
+      let userid = '1'
+      //window.localStorage.getItem('userid');
 
       const [info, setInfo] = useState([]);
 
       useEffect(() => {
-        axios.get("/projectdetail/post/1")
+        axios.get("/projectdetail/post/" + Number(prodNo))
         .then(res => setInfo(res.data))
         .catch(err => console.log(err));
-       }, []);
+       }, [info]);
 
         return ( 
         <>
+            <form>
+              입력<input type="text"ref={postconRef} placeholder="커뮤니티 게시글 작성" />
+              <div style={{display:'none'}}ref={usernoRef}>{userid}</div> 
+              <button onClick={handleSubmit}>작성</button>
+            </form>
             <div>
              프로젝트 {prodNo}의 커뮤니티
              {info.map((post)=>
              <div key={post.postNo}>
                 <div style={{display:'none'}}>게시글번호{post.prodNo}json데이터 테스트입니다
-                {postInfo.postNo}</div>
+                </div>
                 <div>유저이름:{post.maker.userNo}</div>
                 <div>게시글내용:{post.postContent}</div>
-                <div className='postWrite' > 
-                    입력<input type={"text"} placeholder={"커뮤니티 게시글 작성"} defaultValue={post.postCon} onClick={handleChange}></input> 
-                    <button type='submit' onClick={submitClick}>작성</button>
-                </div>
                 </div >
               )}
             </div>
@@ -78,3 +67,4 @@ export default function Post() {
         </> 
         );
 }
+
