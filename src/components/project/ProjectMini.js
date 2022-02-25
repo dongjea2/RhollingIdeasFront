e35e0796adff78124eaeb81e8c104d14680f203b
dongjea2 <dewawd@gmail.com>
2022-02-25 16:42:01 +0900

    프로젝트 미니 수정

--- src/components/project/ProjectMini.js
+++ src/components/project/ProjectMini.js
@@ -1,38 +1,33 @@
 import { Link } from 'react-router-dom';
 import styles from './ProjectMini.module.css';
 
-export default function ProjectMini({ item }) {
-  const {  id ,imgUrl, category,maker, title,
-            ahciveRate } = item;
-
-  console.log(id);
-  console.log(title);
+export default function ProjectMini({ project }) {
+  console.log("프로젝트 미니 속");
+  console.log(project);
 
   return (
     <>
-    <Link to={'/projectdetail/'+Number(id)}>
-    <img className={styles.itemImage} src={imgUrl} alt={title}/>
+    <Link to={'/projectdetail/'+Number( project && project.projectNo)}>
+      <img src ={require(`../../${ project && project.projectImage }`)} className={styles.itemImage}  alt={project && project.projectNo}/>
     </Link>
 
-    {/*<button className={styles.like}></button>*/}
-    <button className={styles.notLike}></button>
+    {project && project.loginedUserProjectInterest === true ? 
+      <button className={styles.like}></button> : <button className={styles.notLike}></button>
+    }
 
     <div className={styles.info}>
 
         <div className={styles.catelink}>
-            <span className={styles.category}> {category} </span>
+            <span className={styles.category}> {project && project.category.categoryName} </span>
             <span className={styles.category}>|</span>
-            <span className={styles.company}> {maker} </span>
+            <span className={styles.company}> {project && project.maker.userName} </span>
         </div>
 
-        <Link to={'/projectdetail/'+Number(id)}>
-        <span className={styles.title}>{title}</span> 
+        <Link to={'/projectdetail/'+Number(project && project.projectNo)}>
+        <span className={styles.title}>{project && project.longTitle}</span> 
         </Link>
-
-
-
         <div className={styles.priceAndPercent}>
-            <span className={styles.percent}>{ahciveRate}% 달성</span>
+            <span className={styles.percent}>50% 달성</span>
         </div>
     </div>
 
