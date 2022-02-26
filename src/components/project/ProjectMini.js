d832b317531b9d80b776aea8038db3a141bf2058
dongjea2 <dewawd@gmail.com>
2022-02-27 00:10:51 +0900

    메인페이지 하트(관심프로젝트 등록)  / 빈하트(관심프로젝트 등록 해제) 기능 추가

--- src/components/project/ProjectMini.js
+++ src/components/project/ProjectMini.js
@@ -1,7 +1,51 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import SimpleModal from '../modal/SimpleModal';
 import styles from './ProjectMini.module.css';
+import heartImg from '../../images/mainpage/heart2.png';
+import emptyHeartImg from '../../images/mainpage/heart.png';
 
 export default function ProjectMini({ project }) {
+
+  //좋아요 버튼
+  const [isLike ,setIsLike ] = useState(project && project.loginedUserProjectInterest);
+  const [buttonDisable , setButtonDisable] = useState(false);
+
+  //좋아요 변경 안내 모달
+  const [modalVisible, setModalVisible] = useState(false)
+  const closeModal = () => { setModalVisible(false) }
+
+
+  const handleDeleteLike= () => {
+    axios.delete('/interest', { 
+      data :{
+      "likeProject": { "projectNo" : project.projectNo},
+      //임시로 만든 유저
+      "likeUser" :{ "userNo" : 1}
+      }, })
+        .then( res=>setIsLike(false))
+        .then(res=>setModalVisible(true))
+        .then(setButtonDisable(true),
+                //하트버튼 1.5초 뒤에 눌리게 설정(서버 과부화 방지)
+                setTimeout(()=>{ setButtonDisable(false); setModalVisible(false); }, 1000))
+         .catch(err => console.log(err));
+
+  }
+
+  const handleAddLike= () => {
+    axios.post('/interest', {
+      "likeProject": { "projectNo" : project.projectNo},
+      //임시로 만든 유저번호
+      "likeUser" :{ "userNo" : 1} })
+        .then( res=>setIsLike(true))
+        .then(res=>setModalVisible(true))
+        .then(setButtonDisable(true),
+                //하트버튼 1.5초 뒤에 눌리게 설정(서버 과부화 방지)
+                setTimeout(()=>{ setButtonDisable(false); setModalVisible(false); }, 1000))
+         .catch(err => console.log(err));
+  }
 
   return (
     <>
@@ -9,8 +53,10 @@ export default function ProjectMini({ project }) {
       <img src ={require(`../../${ project && project.projectImage }`)} className={styles.itemImage}  alt={project && project.projectNo}/>
     </Link>
 
-    {project && project.loginedUserProjectInterest === true ? 
-      <button className={styles.like}/> : <button className={styles.notLike}/>
+    {isLike === true ? 
+        <button className={styles.like} onClick={handleDeleteLike} disabled={buttonDisable}/> 
+        :
+        <button className={styles.notLike} onClick={handleAddLike} disabled={buttonDisable}/>
     }
 
     <div className={styles.info}>
@@ -25,10 +71,40 @@ export default function ProjectMini({ project }) {
         <span className={styles.title}>{project && project.longTitle}</span> 
         </Link>
         <div className={styles.priceAndPercent}>
-            <span className={styles.percent}>50% 달성</span>
+            <span className={styles.percent}>{project && project.achiveRate}% 달성</span>
         </div>
     </div>
+
+        {/*좋아요 수정시 안내 모달*/}
+        {
+            modalVisible && 
+            <SimpleModal visible={modalVisible} closable={true} maskClosable={false} onClose={closeModal}>
+              
+               {isLike === true ?  <Heart/>: <EmptyHeart/> }
+            </SimpleModal>
+        }
 
     </>
   );
 }
+
+const Heart= styled.button`
+  position: relative;
+  top: 0px;
+  left: 0px;
+  width: 100px;
+  height: 100px;
+  background: url(${heartImg}) center center / 100% no-repeat;
+
+`
+
+const EmptyHeart= styled.button`
+ position: relative;
+  top: 0px;
+ left: 0px;
+
+ width: 100px;
+ height: 100px;
+background: url(${emptyHeartImg}) center center / 100% no-repeat;
+
+`
