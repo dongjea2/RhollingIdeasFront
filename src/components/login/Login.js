161206adf0834918c16ce80a24fc59dc92cd11d5
Jihoon Oh <bs4079.dev@gmail.com>
2022-02-28 14:17:40 +0900

    로그인 정보 localStorage에서 sessionStorage로 변경. (key = 'loginedId', 'userNo', 'userUrl')

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,83 +1,86 @@
-import  rholling  from '../../images/mainpage/rholling.PNG';
-import { Link } from 'react-router-dom';
-import './Login.css';
-import { useRef } from 'react';
+import rholling from "../../images/mainpage/rholling.PNG";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import { useRef } from "react";
 
 export default function Login() {
+  let loginedId = window.localStorage.getItem("userId");
+  console.log(loginedId);
 
-    let userId = 'id1';
-    const fakeSubmit = (e)=>{
-        e.preventDefault();
-        window.localStorage.setItem('userId', userId);
-    }
+  const idRef = useRef(null);
+  const pwdRef = useRef(null);
 
-    function fakeLogout() {
-        window.localStorage.removeItem('userId');
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    let loginedId = window.localStorage.getItem('userId');
-    console.log(loginedId);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: idRef.current.value,
+        userPwd: pwdRef.current.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 1) {
+          console.log("로그인 성공");
+          console.log(res);
+          window.sessionStorage.setItem("loginedId", res.loginedId);
+          window.sessionStorage.setItem("userNo", res.userNo);
+          window.sessionStorage.setItem("userUrl", res.userUrl);
+          window.location.replace("/");
+        } else {
+          alert("로그인 실패");
+        }
+      });
+  }
 
-
-    const idRef = useRef(null);
-    const pwdRef = useRef(null);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        let id = idRef.current.value;
-        let pwd = pwdRef.current.value;
-        console.log(id + " " + pwd);
-
-        fetch('/login', {
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify({
-                userId : id,
-                userPwd : pwd
-            })
-        })
-        .then(data => data.json())
-    }
-
-    return (
-    <div className='login-page'>
-        {/* 상단 로고(클릭시 메인페이지 이동) */}
-        <div className="logo">
+  return (
+    <div className="login-page">
+      {/* 상단 로고(클릭시 메인페이지 이동) */}
+      <div className="logo">
         <a href="/">
-            <img src={rholling}/>
+          <img src={rholling} />
         </a>
+      </div>
+      {/* 로그인 */}
+      <div className="login_box">
+        <div className="login_box2">
+          <h3>로그인</h3>
+          <div className="sns_login">
+            <button>페이스북 아이디로 로그인</button>
+            <button>네이버 아이디로 로그인</button>
+            <button>카카오톡 아이디로 로그인</button>
+          </div>
+          <hr />
+          <form className="email_login">
+            <input
+              type="text"
+              ref={idRef}
+              placeholder="이메일 주소 입력"
+              required
+            />
+            <input
+              type="password"
+              ref={pwdRef}
+              placeholder="비밀번호 입력"
+              required
+            />
+            <button onClick={handleSubmit}>로그인</button>
+          </form>
+          <div className="login_text">
+            아직 Rholling ideas 계정이 없으신가요?
+            <Link to={"/signup"}>가입하기</Link>
+          </div>
+          <hr />
+          <div className="login_text">
+            <Link to={""}>혹시 비밀번호를 잊으셨나요?</Link>
+          </div>
         </div>
-        <button onClick={fakeSubmit}>가짜 로그인 버튼</button>
-        <button onClick={fakeLogout}>가짜 로그아웃 버튼</button>
-        {/* 로그인 */}
-        <div className="login_box">
-            <div className="login_box2">
-                <h3>로그인</h3>
-                <div className="sns_login">
-                        <button>페이스북 아이디로 로그인</button>
-                        <button>네이버 아이디로 로그인</button>
-                        <button>카카오톡 아이디로 로그인</button>
-
-                </div>
-                <hr/>
-                <form className= "email_login">
-                    <input type="text" ref={idRef} placeholder="이메일 주소 입력" required/>
-                    <input type="password" ref={pwdRef} placeholder="비밀번호 입력" required/>
-                    <button onClick={handleSubmit}>로그인</button>
-                </form>
-                <div className="login_text">
-                    아직 Rholling ideas 계정이 없으신가요?  
-                    <Link to={'/signup'}>가입하기</Link>
-                </div>
-                <hr/>
-                <div className="login_text">
-                    <Link to={''}>혹시 비밀번호를 잊으셨나요?</Link>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    )
+  );
 }
