5601079ea0fd0b177042cb1c2f6e09bb422f6f59
dongjea2 <dewawd@gmail.com>
2022-03-01 04:42:11 +0900

    UserInfo Styed로 css 대체

--- src/components/order/userInfo/UserInfo.js
+++ src/components/order/userInfo/UserInfo.js
@@ -1,17 +1,37 @@
-import styles from './UserInfo.module.css';
+import styled from "styled-components";
 
 export default function UserInfo({project}){
-
-
     return(
         <>
-        <div className={styles.title}>
-        <h3>후원자 정보</h3>
-        </div>
-        <div className={styles.boxForm}>
-            <div className={styles.boxItem}>연락처 : {project&& project.maker.userPhone} </div>
-            <div className={styles.boxItem}>이메일 : {project&& project.maker.userId} </div>
-        </div>
+        <Title>
+            <h3>후원자 정보</h3>
+        </Title>
+        <Box>
+            <Item> 연락처 : {project&& project.maker.userPhone} </Item>
+            <Item> 이메일 : {project&& project.maker.userId} </Item>
+        </Box>
 </>
     );
 }
+
+
+
+//=====================================
+//Styled Componets
+const Title = styled.div`
+    margin-top: 20px;
+    margin-bottom: 10px;
+`
+
+const Box= styled.div`
+    border: 1px solid darkgrey;
+    border-radius: 4px;
+    width: 730px;
+    margin-top: 5px;
+}
+`
+const Item= styled.div`
+    margin-left: 10px;
+    margin-top: 3px;
+    margin-bottom: 3px;
+`
