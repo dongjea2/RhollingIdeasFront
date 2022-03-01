09753de5b6074de12470cbad1a7f450c2f5f3fbe
hajeongryu <tgkwjdt@gmail.com>
2022-03-02 08:45:44 +0900

    관심프로젝트 하트클릭 이벤트

--- src/components/profile/interest/PreLaunchedProjectList.js
+++ src/components/profile/interest/PreLaunchedProjectList.js
@@ -1,10 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styles from '../Profile.module.css';
 
 export default function PreLaunchedProjectList(){
     const location = useLocation();
-    const pres = location.state.pres;
+    const alarms = location.state.alarms;
     const intersLength = location.state.intersLength;
+    const [cnt, setCnt] = useState(0);
+
+    //좋아요 버튼
+    const [buttonDisable , setButtonDisable] = useState(false);
+    //좋아요 변경 안내 모달
+    const [modalVisible, setModalVisible] = useState(false)
+    const closeModal = () => { setModalVisible(false) }
+
+    //a.좋아요 해제
+    const handleDeleteLike= (projectNo) => {
+      axios.delete('/interest', { 
+        data :{
+        "likeProject": { "projectNo" : projectNo},
+        //임시로 만든 유저
+        "likeUser" :{ "userNo" : window.sessionStorage.getItem("userNo")}
+        }, }
+        ).then(function(response){
+            console.log(response);
+            setCnt(cnt + 1);
+        })
+        .then(res=>setModalVisible(true))
+        .then(setButtonDisable(true),
+                  //하트버튼 1.5초 뒤에 눌리게 설정(서버 과부화 방지)
+                  setTimeout(()=>{ setButtonDisable(false); setModalVisible(false); }, 1000))
+        .catch(err => alert("로그인 하세요"));
+
+    }
+
     return(
         <section>
             <div className={styles.Header}>
@@ -14,39 +44,38 @@ export default function PreLaunchedProjectList(){
                         <Link to="/interestlist" replace={true} className={styles.notSelectedA}>좋아한 {intersLength}</Link>
                     </span>
                     <span className={styles.selectedSpan}>
-                        <Link to="/prelaunchedlist" state={{pres: pres, intersLength: intersLength}} replace={true} className={styles.selectedA} style={{color: "black"}}>알림신청 {pres.length}</Link>
+                        <Link to="/prelaunchedlist" state={{alarms: alarms, intersLength: intersLength}} replace={true} className={styles.selectedA} style={{color: "black"}}>알림신청 {alarms.length}</Link>
                     </span>
                 </div>
             </div>
             <div className={styles.selectContent}>
-                <br/><br/>
                 {
-                    pres.length === 0 ?
+                    alarms.length === 0 ?
                 <div className={styles.noContent}>
                     <img src={require('../../../images/profile/empty alarm.png')} alt="no_like_project" />
                     <div>알림신청한 프로젝트가 없습니다.</div>
                 </div> :
                 
                 <div className={styles.Content}>
-                {pres.map((pre) => (
-                    <div className={styles.item} key={pre.likeProject.projectNo}>
-                        <Link to={'/projectdetail/'+Number(pre.likeProject.projectNo)}>
-                            <img className={styles.itemImage} src={require(`../../../${pre.likeProject.projectImage}`)} alt={pre.likeProject.projectNo}/>
+                {alarms.map((alarm) => (
+                    <div className={styles.item} key={alarm.projectNo}>
+                        <Link to={'/projectdetail/'+Number(alarm.projectNo)}>
+                            <img className={styles.itemImage} src={require(`../../../${alarm.projectImage}`)} alt={alarm.projectNo}/>
                         </Link>
 
                         <div className={styles.info}>
 
                             <div className={styles.catelink}>
-                                <span className={styles.category}> {pre.likeProject.category.categoryName} | {pre.likeProject.maker.userName} </span>
+                                <span className={styles.category}> {alarm.categoryName} | {alarm.userName} </span>
                             </div>
 
-                            <Link to={'/projectdetail/'+Number(pre.likeProject.projectNo)}>
-                            <span className={styles.title}>{pre.likeProject.longTitle}</span> 
+                            <Link to={'/projectdetail/'+Number(alarm.projectNo)}>
+                            <span className={styles.title}>{alarm.longTitle}</span> 
                             </Link>
-                            <span className={styles.brief}>{pre.likeProject.projectBrief}</span>
+                            <span className={styles.brief}>{alarm.projectBrief}</span>
                         </div>
                         <div className={styles.alarmButtonWrap}>
-                            <button className={styles.alarmButton}>알림신청 중</button>
+                            <button className={styles.alarmButton} onClick={(e) => handleDeleteLike(alarm.projectNo, e)} disabled={buttonDisable}>알림신청 중</button>
                         </div>
                     </div>
                 ))}
