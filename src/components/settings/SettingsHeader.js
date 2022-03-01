e64b1d6a8828d60c8fff46ad6bf050511baa2329
Jihoon Oh <bs4079.dev@gmail.com>
2022-03-02 04:51:48 +0900

    배송지 추가 수정 삭제 구현 / CSS 추가

--- src/components/settings/SettingsHeader.js
+++ src/components/settings/SettingsHeader.js
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
-import './SettingsHeader.css'
+import "./SettingsHeader.css";
 
 export default function SettingsHeader() {
-    return (
-        <div className="setting-header">
+  return (
+    <div className="setting-header">
+      <div className="setting-title">
         <h1>설정</h1>
-        <ul className="settings_list">
-            <li><Link to={'/profile/accountset'}>계정</Link></li>
-            <li><Link to={'/profile/paymentset'}>결제수단</Link></li>
-            <li><Link to={'/profile/addressset'}>배송지</Link></li>
-        </ul>
-        </div>
-    )
+      </div>
+      <ul className="settings_list">
+        <li>
+          <Link to={"/profile/accountset"}>계정</Link>
+        </li>
+        <li>
+          <Link to={"/profile/paymentset"}>결제수단</Link>
+        </li>
+        <li>
+          <Link to={"/profile/addressset"}>배송지</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
