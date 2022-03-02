62eeaffc43475387b7e29095c18127d65d457a85
Jihoon Oh <bs4079.dev@gmail.com>
2022-03-02 09:42:27 +0900

    logout 세션 키값 수정

--- src/components/login/Logout.js
+++ src/components/login/Logout.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function Logout() {
   axios.post("/logout");
-  window.sessionStorage.removeItem("userId");
+  window.sessionStorage.removeItem("loginedId");
   window.sessionStorage.removeItem("userNo");
   window.sessionStorage.removeItem("userUrl");
   window.sessionStorage.removeItem("userName");
