0d6371c5fcb7275d74e463ca237b1c4323d8e095
2reesh <dltkdgns1125@gmail.com>
2022-03-02 11:20:02 +0900

    프로젝트 올리기부분 수정

--- src/components/project/ProjectWrite.js
+++ src/components/project/ProjectWrite.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { data } from "browserslist";
 import React,{Component, useEffect, useRef, useState} from "react";
 import SimpleModal from "../modal/SimpleModal";
+import './ProjectWrite.css';
 
 export default function ProjectWrite(){
     const [category, setCategory]= useState([]);
@@ -19,8 +21,6 @@ export default function ProjectWrite(){
     const projectcontentRef = useRef(null);
     const projecturlRef = useRef(null);
 
-
-
     const rewardPriceRef = useRef(null);
 	const rewardNameRef = useRef(null);
 	const deliverDateRef = useRef(null);
@@ -29,41 +29,13 @@ export default function ProjectWrite(){
 	const deliverSelectRef = useRef(null);
 
 
-    function onSubmit(e){
-        e.preventDefault();
-        
-   
-    let data = {
-            userNo : usernoRef.current.value,
-            category: categoryRef.current.value,
-            longtitle: longtitleRef.current.value,
-            projectbrief: projectbriefRef.current.value,
-            editorpick: editorpickRef.current.value,            
-            projectimage: projectimageRef.current.value,
-            targetprice: targetpriceRef.current.value,
-            startdate: startdateRef.current.value,
-            enddate: enddateRef.current.value,
-            shorttitle: shorttitleRef.current.value,            
-            projectcontent: projectcontentRef.current.value,
-            projecturl: projecturlRef.current.value}
-    }
-
-    // let rewarddata = { 
-    //      rewardPrice: rewardPriceRef.current.value,
-    //      rewardName: rewardNameRef.current.value,
-    //      deliverDate: deliverDateRef.current.value,
-    //      rewardNum: rewardNumRef.current.value,
-    //      itemName: itemNameRef.current.value,
-    //      deliverSelect: deliverSelectRef.current.value
-    // }
-
     const [showModal, setShowModal] = useState(false);
     const openMadal = ()=>{
         setShowModal((prev)=>!prev);
     };
 
 
-    const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(true);
     
     const closeModal = () => {
         setModalVisible(false) 
@@ -75,7 +47,6 @@ export default function ProjectWrite(){
       useEffect(() => {
         axios.get("/category")
         .then(res => setInfo(res.data))
-        //여기서 모달창에 입력한 reward뿌려야함
         .catch(err => console.log(err));
        }, [plag]);
 
@@ -86,49 +57,108 @@ export default function ProjectWrite(){
        };
 
 
+       console.log(text);
 
        
-       const handleSubmit = async (e) => {
+       const onSubmit = async (e) => {
         e.preventDefault();
-        
-        let data = {
+           let category = text;
+           let longtitle = longtitleRef.current.value;
+           let projectbrief = projectbriefRef.current.value;
+           let editorpick = '0';
+           let projectimage = '1';
+           let targetprice = targetpriceRef.current.value;
+           let startdate = startdateRef.current.value;
+           let enddate = enddateRef.current.value;
+           let shorttitle = shorttitleRef.current.value;
+           let projectcontent = projectcontentRef.current.value;
+           let projecturl = projecturlRef.current.value;
+
+           let rewardPrice = rewardPriceRef.current.value;
+           let rewardName = rewardNameRef.current.value;
+           let deliverDate = deliverDateRef.current.value;
+           let rewardNum = rewardNumRef.current.value;
+           let itemName = itemNameRef.current.value;
+
             //reward, category, project 다 보내야함
+        let postdata = {
+                maker : {userNo: 1},
+                category: {categoryNo: category},
+                longTitle: longtitle,
+                projectBrief: projectbrief,
+                // projectImage: projectimage,
+                targetPrice: targetprice,
+                shortTitle: shorttitle,            
+                projectContent: projectcontent,
+                startDate: startdate,
+                endDate: enddate,
+                projectUrl: projecturl,
+                reward: {rewardName: rewardName},
+                reward: {rewardPrice: rewardPrice},
+                reward: {deliverDate: deliverDate},
+                reward: {rewardNum: rewardNum},
+                reward: {itemName: itemName},
         }
 
-        axios.post("/write",  JSON.stringify(data), {
+        let formData = new FormData();
+        let files = e.target.img.files;
+        formData.append("file", files[0]);
+
+        formData.append("data", JSON.stringify(postdata));
+
+        axios.post("/projectwrite", formData , {
           headers: {
-            "Content-Type": `application/json`,
+            "Content-Type": `multipart/form-data`,
           },
+          data: formData
         }).then((res) => {
           console.log(res);
           setPlag(plag + 1);
         }).catch(err => console.log(err));
+
+        
+
+        console.log(postdata);
+        
       }
 
+
     return(
-        <form onSubmit={onSubmit}>
-            <select ref={categoryRef}>
+        <form onSubmit={(e) => onSubmit(e)}>
+            <select className="categoryref" ref={categoryRef} onClick={onChange}>
+                {/* 카테고리 name으로 선택하는데 프로젝트 작성시 카테고리no로 저장함 */}
                 {info.map((category)=>
-                <option key={category.categoryNo}>
+                <option key={category.categoryNo} value={category.categoryNo}>
                     {category.categoryName}  
                 </option>
                 )}
-            </select>
+                </select>
+            
             <div className="input">
             <div style={{display:'none'}}>projectNo</div>
             <div style={{display:'none'}}>userNo</div>
-            
+            {/* <input type="text"> {text}</input> */}
+            <div>{text}</div>
+            <label>프로젝트 제목을 설정하는 부분입니다</label>
             <input type="text" ref={longtitleRef} placeholder="longtitle"/>
+            <label>프로젝트 어쩌구를 설정하는 부분입니다</label>
             <input type="text" ref={projectbriefRef} placeholder="projectbrief"/>
-            <div>editorpick</div>
+            <input type="file" accept="image/*" name="img"/>
             <div>projectimage</div>
             <input type="number" ref={targetpriceRef} placeholder="targetprice"/>
             <input type="text" ref={shorttitleRef} placeholder="shorttitle" />
             <input type="text" ref={projectcontentRef} placeholder="projectcontent"/>
             <input type="text" ref={projecturlRef} placeholder="projecturl"/>
-            
+            <input type="date" ref={startdateRef}/>
+            <input type="date" ref={enddateRef}/>
+
 
             <div>선물 추가부분
+                    <input type="text" ref={rewardNameRef} placeholder="선물이름" />
+                    <input type="text" ref={rewardPriceRef} placeholder="선물금액" />
+                    <input type="text" ref={rewardNumRef} placeholder="선물한정수량" />
+                    <input type="text" ref={itemNameRef} placeholder="아이템 이름" />
+                    <input type="number" ref={deliverDateRef} placeholder="예상전달일"/>
                 <button onClick={openMadal}></button>
 
                 {
@@ -146,9 +176,9 @@ export default function ProjectWrite(){
                 </SimpleModal>
                 }
             </div>
-            <button onClick={handleSubmit}>프로젝트 작성</button>
+            <button type="submit">프로젝트 작성</button>
             </div>
-        </form>      
+        </form >      
         
     );
 }
