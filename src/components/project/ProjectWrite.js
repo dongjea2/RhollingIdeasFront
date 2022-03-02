b4a1142d3bce77619fcc09ce4daec31e501b7d27
dongjea2 <dewawd@gmail.com>
2022-03-02 11:45:43 +0900

    모달 on off 추가

--- src/components/project/ProjectWrite.js
+++ src/components/project/ProjectWrite.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { data } from "browserslist";
 import React,{Component, useEffect, useRef, useState} from "react";
+import styled from "styled-components";
 import SimpleModal from "../modal/SimpleModal";
 import './ProjectWrite.css';
+
 
 export default function ProjectWrite(){
     const [category, setCategory]= useState([]);
@@ -30,12 +32,20 @@ export default function ProjectWrite(){
 
 
     const [showModal, setShowModal] = useState(false);
+
+    //모달 visible
+    const [modalVisible, setModalVisible] = useState(false);    
+
     const openMadal = ()=>{
-        setShowModal((prev)=>!prev);
+        setModalVisible(true);
+    };
+
+    const closeMadal = ()=>{
+        setModalVisible(false);
     };
 
 
-    const [modalVisible, setModalVisible] = useState(true);
+
     
     const closeModal = () => {
         setModalVisible(false) 
@@ -153,13 +163,10 @@ export default function ProjectWrite(){
             <input type="date" ref={enddateRef}/>
 
 
-            <div>선물 추가부분
-                    <input type="text" ref={rewardNameRef} placeholder="선물이름" />
-                    <input type="text" ref={rewardPriceRef} placeholder="선물금액" />
-                    <input type="text" ref={rewardNumRef} placeholder="선물한정수량" />
-                    <input type="text" ref={itemNameRef} placeholder="아이템 이름" />
-                    <input type="number" ref={deliverDateRef} placeholder="예상전달일"/>
-                <button onClick={openMadal}></button>
+            <div>
+                <ModalBtn onClick={openMadal}>
+                    선물 추가부분
+                </ModalBtn>                    
 
                 {
                 modalVisible && <SimpleModal
@@ -173,6 +180,7 @@ export default function ProjectWrite(){
                     <input type="text" ref={itemNameRef} placeholder="아이템 이름"/>
                     <input type="number" ref={deliverSelectRef} placeholder="예상전달일"/>
                     <button >확인</button>
+                <ModalBtn onClick={closeMadal}>취소</ModalBtn>
                 </SimpleModal>
                 }
             </div>
@@ -182,3 +190,32 @@ export default function ProjectWrite(){
         
     );
 }
+
+
+const ModalBtn = styled.button`
+    font-size: 23px;
+    margin-top: 30px;
+    min-width: 120px;
+    width: auto;
+    height: 50px;
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    white-space: nowrap;
+    border-radius: 4px;
+    margin: 0px;
+    border: 0px;
+    margin-left:20px;
+    outline: none;
+    font-weight: normal;
+    box-sizing: border-box;
+    background-color: rgb(255, 87, 87);
+    color: rgb(255, 255, 255);
+
+  &:hover {
+    background-color: rgb(245, 77, 67);
+    color: black;
+
+`
